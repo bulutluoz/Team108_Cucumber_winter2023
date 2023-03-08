80b23dc683a0e5046d9b4410877cda272c5f12c8
ahmet bulutluoz <abbulutluoz@gmail.com>
2023-03-08 10:18:49 +0100

    14.gun sonu

---
+++ src/test/java/stepdefinitions/AmazonStepdefinitions.java
@@ -0,0 +1,24 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class AmazonStepdefinitions {
+
+    @Given("kullanici Amazon anasayfaya gider")
+    public void kullanici_amazon_anasayfaya_gider() {
+
+    }
+    @Then("arama kutusuna Nutella yazar ve enter tusuna basar")
+    public void arama_kutusuna_nutella_yazar_ve_enter_tusuna_basar() {
+
+    }
+    @Then("arama sonuclarinin Nutella icerdigini test eder")
+    public void arama_sonuclarinin_nutella_icerdigini_test_eder() {
+
+    }
+    @Then("sayfayi kapatir")
+    public void sayfayi_kapatir() {
+
+    }
+}
