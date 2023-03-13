3aea6f4e678e4f769ed5c79922ca38a883f27b26
ahmet bulutluoz <abbulutluoz@gmail.com>
2023-03-13 10:40:27 +0100

    15.gun sonu

--- src/test/java/stepdefinitions/QdStepdefinitions.java
+++ src/test/java/stepdefinitions/QdStepdefinitions.java
@@ -1,26 +1,47 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import pages.QdPage;
+import utilities.ConfigReader;
 
 public class QdStepdefinitions {
+    QdPage qdPage = new QdPage();
     @Then("ilk login linkine tiklar")
     public void ilk_login_linkine_tiklar() {
-
+        qdPage.ilkLoginLinki.click();
     }
     @Then("username olarak {string} girer")
-    public void username_olarak_girer(String string) {
-
+    public void username_olarak_girer(String username) {
+        qdPage.emailKutusu.sendKeys(ConfigReader.getProperty(username));
     }
     @Then("password olarak {string} girer")
-    public void password_olarak_girer(String string) {
-
+    public void password_olarak_girer(String password) {
+        qdPage.passwordKutusu.sendKeys(ConfigReader.getProperty(password));
     }
     @Then("login butonuna basar")
     public void login_butonuna_basar() {
-
+        qdPage.loginButonu.click();
     }
     @Then("giris yapilamadigini test eder")
     public void giris_yapilamadigini_test_eder() {
+        Assert.assertTrue(qdPage.emailKutusu.isEnabled());
+    }
 
+    @Then("basarili olarak giris yapildigini test eder")
+    public void basariliOlarakGirisYapildiginiTestEder() {
+
+        Assert.assertTrue(qdPage.basariliGirisElementi.isDisplayed());
+    }
+
+    @And("username olarak examples dan {string} girer")
+    public void usernameOlarakExamplesDanGirer(String username) {
+        qdPage.emailKutusu.sendKeys(username);
+    }
+
+    @And("password olarak examples dan {string} girer")
+    public void passwordOlarakExamplesDanGirer(String password) {
+        qdPage.passwordKutusu.sendKeys(password);
     }
 }
--- target/test-classes/features/US1007_ScenarioOutlineNegativeLoginTest.feature
+++ target/test-classes/features/US1007_ScenarioOutlineNegativeLoginTest.feature
@@ -1,11 +1,11 @@
 Feature: US1007 Scenario Outline ile coklu negative login test
 
-  @wip
+  @so
   Scenario Outline: TC11 kullanici verilen listedeki kullanici isimleri ile giris yapamamali
     Given kullanici "qdUrl" anasayfaya gider
     Then ilk login linkine tiklar
-    And username olarak "<username>" girer
-    And password olarak "<password>" girer
+    And username olarak examples dan "<username>" girer
+    And password olarak examples dan "<password>" girer
     And login butonuna basar
     Then giris yapilamadigini test eder
     And sayfayi kapatir
